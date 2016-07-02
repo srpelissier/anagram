697bce14e927e06f06efacf32e788ba8bf3ec990
Sylvain Pélissier <srpelissier@gmail.com>
2016-07-02 23:47:45 +0200

    ui: capital letters.

--- start.rb
+++ start.rb
@@ -7,17 +7,33 @@ def makearray(file)
 end
 
 @wordlist = makearray "./data/wordlist.txt"
+@alphabet = @wordlist.join { |word|
+	word.chomp
+}.chars
+alphabet_weights = Hash.new(0)
+@alphabet.each do |v|
+	alphabet_weights[v] += 1
+end
+alphabet_weights.each { |k,v|
+	alphabet_weights[k] = (alphabet_weights[k].to_f/@alphabet.length*1000).to_i
+}
+@alphabet = []
+("a" .. "z").to_a.each { |alpha|
+	@alphabet << Array.new(alphabet_weights[alpha], alpha)
+}
+@alphabet.flatten!
 
 while end_game == false do
 
 	change_letters = false
 	def getletters(n)
-		alphabet = ("a" .. "z").to_a
+
 		ret = []
 		for i in 0...n do
-			ret << alphabet.sample
+			ret << @alphabet.sample
 		end
-		ret.sort
+		ret#.sort
+
 	end
 
 	top10 = []
@@ -36,7 +52,11 @@ while end_game == false do
 	baseletters = getletters(15)
 
 	def printletters(letters)
-		puts "\nLetters: #{letters.join(" ")}\n"
+		copy = []
+		letters.each { |l|
+			copy << l.upcase
+		}
+		puts "\nLetters: #{copy.join(" ")}\n"
 	end
 
 	while change_letters == false && end_game == false do
@@ -51,15 +71,16 @@ while end_game == false do
 
 		letters = word.chars.sort
 
+		error = ""
 		letters.each { |letter|
 			if !baseletters.include? letter
 				@word_is_valid = false
-				msg += "\n#{letter} not found."
+				error = letter
 			end
 		}
+		msg += "\n'#{error}' not found." unless @word_is_valid
 
-		if @word_is_valid == true
-
+		if @word_is_valid
 			letters.each { |letter|
 				letters_copy = letters.dup
 				baseletters_copy = baseletters.dup
@@ -67,31 +88,30 @@ while end_game == false do
 				qty_baseletter = baseletters_copy.keep_if { |baseletter_copy| baseletter_copy == letter }.length
 				if qty_letter > qty_baseletter
 					@word_is_valid = false
-
+					error = letter
 				end
 			}
-
-			msg += "\nYou used too many '#{letter}'." if @word_is_valid == false
+			msg += "\nYou used too many '#{error}'." unless @word_is_valid
 
 		end
 
-		if (@wordlist.include? word) && (@word_is_valid == true)
+		if (@wordlist.include? word) && @word_is_valid
 			score = word.size
-		elsif @word_is_valid == true
+		elsif @word_is_valid
 			@word_is_valid = false
-			msg += "\n#{word} unknown."
+			msg += "\n'#{word}' unknown."
 		end
 
-		if @word_is_valid == true
+		if @word_is_valid
 			top10.each { |topscore|
 				if topscore[1] == word
 					@word_is_valid = false
-					msg += "\n#{word} has been proposed."
+					msg += "\n'#{word}' has been proposed."
 				end
 			}
 		end
 
-		if @word_is_valid == true
+		if @word_is_valid
 			top10 << [score,word]
 			top10.sort_by! { |rank| rank[0] }
 			top10 = top10.uniq { |rank| rank[1] }
@@ -101,17 +121,15 @@ while end_game == false do
 
 		puts "\n#{word} wins #{score} points.#{msg}"
 
-		printranks top10
+		printranks top10 if top10.length > 0
 
-		puts "\nNew (A)ttempt) - New (L)etters?\n"
+		puts "\nHit [RETURN] to try again or [N] for new letters. [Q] to quit."
 
 		resp = gets.chomp
 		case resp
-		when "A", "a"
-			change_letters = false
-		when "L", "l"
+		when "n", "N"
 			change_letters = true
-		else
+		when "q", "Q"
 			end_game = true
 		end
 
